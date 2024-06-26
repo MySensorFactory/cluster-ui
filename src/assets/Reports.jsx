bfbcfafa355319ea15fa34482bb62c693daf277c
damiano <macmacroll@gmail.com>
2024-06-26 14:08:12 +0200

    Added Reports.jsx

---
+++ src/assets/Reports.jsx
@@ -0,0 +1,15 @@
+import * as React from "react";
+const Report = (props) => (
+    <svg
+        fill="#FFFFFF"
+        width="1em"
+        height="1em"
+        viewBox="0 0 32 32"
+        xmlns="http://www.w3.org/2000/svg"
+        {...props}
+    >
+        <title>{"report"}</title>
+        <path d="M6 11h4v17h-4v-17zM22 16v12h4v-12h-4zM14 28h4v-24h-4v24z" />
+    </svg>
+);
+export default Report;
