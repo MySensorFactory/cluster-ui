da6a28a5d748b4b6337228ece496149604f81373
damiano <macmacroll@gmail.com>
2024-06-26 15:18:52 +0200

    Improved Header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,18 +1,28 @@
 import React from 'react';
 import styled from 'styled-components';
+import DataCircle from "../assets/DataCircle";
+import SvgResizer from "react-svg-resizer";
 
 const HeaderContainer = styled.div`
-  background-color: #2e2e2e;
-  color: white;
-  padding: 20px;
-  display: flex;
-  align-items: center;
-  justify-content: space-between;
+    background-color: #1C1C21;
+    color: white;
+    padding: 30px;
+    display: flex;
+    gap: 10px;
+    flex-direction: row;
+    justify-content: flex-start;
+    align-items: center;
+    font-family: Inter, sans-serif;
+    height: 1%;
+    border-bottom: 1px solid white;
 `;
 
 function Header() {
     return (
         <HeaderContainer>
+            <SvgResizer size={25}>
+                <DataCircle/>
+            </SvgResizer>
             <h1>Factory Data Analytics</h1>
         </HeaderContainer>
     );
