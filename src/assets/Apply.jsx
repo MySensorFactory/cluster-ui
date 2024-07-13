cb77be51d7110a9a82f7a23eeb0e63e491b55269
damiano <macmacroll@gmail.com>
2024-07-13 15:59:49 +0200

    Added Apply.jsx component

---
+++ src/assets/Apply.jsx
@@ -0,0 +1,28 @@
+import * as React from "react";
+const Apply = (props) => (
+    <svg
+        viewBox="0 0 24 24"
+        role="img"
+        xmlns="http://www.w3.org/2000/svg"
+        aria-labelledby="okIconTitle"
+        stroke="#FFFFFF"
+        strokeWidth={2}
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        fill="none"
+        color="#FFFFFF"
+        {...props}
+    >
+        <g id="SVGRepo_bgCarrier" strokeWidth={0} />
+        <g
+            id="SVGRepo_tracerCarrier"
+            strokeLinecap="round"
+            strokeLinejoin="round"
+        />
+        <g id="SVGRepo_iconCarrier">
+            <title id="okIconTitle">{"Ok"}</title>
+            <polyline points="4 13 9 18 20 7" />
+        </g>
+    </svg>
+);
+export default Apply;
