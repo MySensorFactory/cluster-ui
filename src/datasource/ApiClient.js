2df1a83af8a0fa9e1ed417b5242906ab0e0fe1e8
damiano <macmacroll@gmail.com>
2024-08-06 21:52:47 +0200

    Added more methods to ApiClient.js

--- src/datasource/ApiClient.js
+++ src/datasource/ApiClient.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 export function useApi() {
 
-    const baseURL = 'http://localhost:8080';
+    const baseURL = 'http://localhost:8080/home';
 
     const api = axios.create({
         baseURL,
@@ -41,11 +41,52 @@ export function useApi() {
         },
 
         getChartData: (sensorType, timeRange, onComplete) => {
-            api.get('/chart-data', {
-                params: {sensorType, timeRange},
-            })
+            api.get('/chart-data', {params: {sensorType, timeRange},}
+            )
                 .then(r => nullSafeOnComplete(r, onComplete))
                 .catch(handleError);
+        },
+
+        addSensorValue: (sensorValue, onComplete) => {
+            api.post('/sensor-values', sensorValue)
+                .then(r => nullSafeOnComplete(r, onComplete))
+                .catch(handleError)
+        },
+
+        updateSensorValue: (index, sensorValue, onComplete) => {
+            api.put(`/sensor-values/${index}`, sensorValue)
+                .then(r => nullSafeOnComplete(r, onComplete))
+                .catch(handleError)
+        },
+
+        deleteSensorValue: (index, onComplete) => {
+            api.delete(`/sensor-values/${index}`)
+                .then(r => nullSafeOnComplete(r, onComplete))
+                .catch(handleError);
+        },
+
+        addChartConfig: (chartConfig, onComplete) => {
+            api.post('/chart-configs', chartConfig)
+                .then(r => nullSafeOnComplete(r, onComplete))
+                .catch(handleError)
+        },
+
+        updateChartConfig: (index, chartConfig, onComplete) => {
+            api.put(`/chart-configs/${index}`, chartConfig)
+                .then(r => nullSafeOnComplete(r, onComplete))
+                .catch(handleError);
+        },
+
+        deleteChartConfig: (index, onComplete) => {
+            api.delete(`/chart-configs/${index}`)
+                .then(r => nullSafeOnComplete(r, onComplete))
+                .catch(handleError)
+        },
+
+        getChartConfigs: (onComplete) => {
+            api.get('/chart-configs')
+                .then(r => nullSafeOnComplete(r, onComplete))
+                .catch(handleError)
         },
 
     }
